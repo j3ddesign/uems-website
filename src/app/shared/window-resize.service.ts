de52f5d592baa0f8c0409938efabbf70d87e0614
Jordan Ide <jorrdandied@hotmail.com>
2016-12-22 19:16:31 +0000

    fix resize bug

--- src/app/shared/window-resize.service.ts
+++ src/app/shared/window-resize.service.ts
@@ -1,6 +1,14 @@
 import { Observable, BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
+
+const getWindowSize = () => {
+  return {
+    height: window.innerHeight,
+    width: window.innerWidth
+  };
+};
+
 @Injectable()
 export class WindowSize {
   IS_MOBILE: BehaviorSubject<boolean> = new BehaviorSubject(false);
@@ -10,7 +18,7 @@ export class WindowSize {
   private mobileWidth: number = 300;
 
   constructor() {
-    const windowSize$ = createWindowSize$();
+    const windowSize$ = this.createWindowSize$();
 
     // Expose a simple observable of the width
     this.width$ = (windowSize$.pluck('width') as Observable<number>)
@@ -27,24 +35,20 @@ export class WindowSize {
         (height, width) => ({
           height: height,
           width: width
-        }))
+        }));
+  }
+  createWindowSize$() {
+    return Observable.fromEvent(window, 'resize')
+      .map(getWindowSize) // map each event to a simple object of {height, width}
+      .do(windowSize => {
+        this.IS_MOBILE.next(windowSize > this.mobileWidth);
+      })
+      .startWith(getWindowSize()) // initialize values on page load
+      .publishReplay(1)
+      .refCount();
   }
 }
 
-const createWindowSize$ = () =>
-  // Create an observable from the window resize event
-  Observable.fromEvent(window, 'resize')
-    .map(getWindowSize) // map each event to a simple object of {height, width}
-    .do(windowSize => {
-      this.IS_MOBILE.next(windowSize > this.mobileWidth);
-    })
-    .startWith(getWindowSize()) // initialize values on page load
-    .publishReplay(1)
-    .refCount();
 
-const getWindowSize = () => {
-  return {
-    height: window.innerHeight,
-    width: window.innerWidth
-  }
-};
+
+
